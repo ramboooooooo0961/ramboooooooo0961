9936eb65378544995a77317fd59ddc0d4fc4c59a
Rambo <155873890+ramboooooooo0961@users.noreply.github.com>
2024-11-28 13:36:02 +0530

    Update hangman.c

--- hangman.c
+++ hangman.c
@@ -1,90 +1,130 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #define MAX_TRIES 6
 
-// Function to print the current state of the word
-void printWord(char word[], char guessed[], int wordLength) {
-    for (int i = 0; i < wordLength; i++) {
-        if (guessed[i] == 1)
-            printf("%c ", word[i]);
-        else
-            printf("_ ");
+// Define a structure to store the word and its category/definition
+typedef struct {
+    char word[100];
+    char category[100];
+} Word;
+
+void printHangman(int tries) {
+    switch (tries) {
+        case 0:
+            printf("\n\n\n\n\n");
+            break;
+        case 1:
+            printf("\n  -----\n  |   |\n  |\n  |\n  |\n  |\n-----\n");
+            break;
+        case 2:
+            printf("\n  -----\n  |   |\n  |   O\n  |\n  |\n  |\n-----\n");
+            break;
+        case 3:
+            printf("\n  -----\n  |   |\n  |   O\n  |   |\n  |\n  |\n-----\n");
+            break;
+        case 4:
+            printf("\n  -----\n  |   |\n  |   O\n  |  /|\n  |\n  |\n-----\n");
+            break;
+        case 5:
+            printf("\n  -----\n  |   |\n  |   O\n  |  /|\\\n  |\n  |\n-----\n");
+            break;
+        case 6:
+            printf("\n  -----\n  |   |\n  |   O\n  |  /|\\\n  |  /\n  |\n-----\n");
+            break;
+        case 7:
+            printf("\n  -----\n  |   |\n  |   O\n  |  /|\\\n  |  / \\\n  |\n-----\n");
+            break;
+        default:
+            break;
     }
-    printf("\n");
 }
 
-// Function to select a random word from a list of words
-void chooseRandomWord(char *word) {
-    char *words[] = {
-        "hangman", "programming", "computer", "developer", "language", "code", "challenge", "function", "debug", "variable"
+// Function to select a random word from the list
+void selectWord(Word *selectedWord) {
+    // Array of words with their categories (hints)
+    Word words[] = {
+        {"programming", "A field of study involving writing code"},
+        {"computer", "An electronic device for processing data"},
+        {"hangman", "A popular word-guessing game"},
+        {"developer", "A person who writes code"},
+        {"puzzle", "A game or problem designed to test ingenuity"}
     };
-    int numWords = sizeof(words) / sizeof(words[0]);
-    srand(time(NULL)); // Initialize random seed
-    int randomIndex = rand() % numWords;
-    strcpy(word, words[randomIndex]); // Copy the randomly selected word to the 'word' variable
+    int wordCount = 5;
+
+    // Select a random word from the list
+    int index = rand() % wordCount;
+    *selectedWord = words[index];
+}
+
+// Function to give a hint (reveal the category of the word)
+void giveHint(Word selectedWord) {
+    printf("\nHint: The word belongs to the category: %s\n", selectedWord.category);
 }
 
 int main() {
-    char word[50]; // To store the selected word
-    chooseRandomWord(word); // Choose a random word
-    int wordLength = strlen(word);
-    
-    char guessed[wordLength]; // Keeps track of guessed letters (0 = not guessed, 1 = guessed)
-    int wrongGuesses = 0;
-    char guessedLetter;
-    int guessedCorrectly;
+    Word selectedWord;
+    char guessedWord[100];
+    int tries = 0;
+    int wordLength;
+    char guess;
+    int gameWon = 0;
 
-    // Initialize guessed array
+    // Seed the random number generator
+    srand(time(NULL));
+
+    // Select a random word
+    selectWord(&selectedWord);
+    wordLength = strlen(selectedWord.word);
+
+    // Initialize the guessed word with underscores
     for (int i = 0; i < wordLength; i++) {
-        guessed[i] = 0; // All letters are initially not guessed
+        guessedWord[i] = '_';
     }
+    guessedWord[wordLength] = '\0';
 
-    printf("Welcome to Hangman!\n");
+    // Game loop
+    while (tries < MAX_TRIES) {
+        printf("\nWord: %s\n", guessedWord);
+        printf("Tries left: %d\n", MAX_TRIES - tries);
+        printf("Would you like a hint? (y/n): ");
+        
+        char hintChoice;
+        scanf(" %c", &hintChoice);
 
-    // Main game loop
-    while (wrongGuesses < MAX_TRIES) {
-        printf("\nGuess the word: ");
-        printWord(word, guessed, wordLength);
+        if (hintChoice == 'y' || hintChoice == 'Y') {
+            giveHint(selectedWord);
+        }
 
         printf("Enter a letter: ");
-        scanf(" %c", &guessedLetter);
-        guessedLetter = tolower(guessedLetter); // Convert input to lowercase for simplicity
+        scanf(" %c", &guess);  // Take user input
 
-        // Check if the letter is in the word
-        guessedCorrectly = 0;
+        // Check if the guessed letter is in the word
+        int found = 0;
         for (int i = 0; i < wordLength; i++) {
-            if (word[i] == guessedLetter && guessed[i] == 0) {
-                guessed[i] = 1; // Mark letter as guessed
-                guessedCorrectly = 1;
+            if (selectedWord.word[i] == guess && guessedWord[i] == '_') {
+                guessedWord[i] = guess;
+                found = 1;
             }
         }
 
-        if (!guessedCorrectly) {
-            wrongGuesses++;
-            printf("Wrong guess! You have %d tries left.\n", MAX_TRIES - wrongGuesses);
+        if (!found) {
+            tries++;
+            printHangman(tries);
         }
 
-        // Check if the player has guessed all letters
-        int allGuessed = 1;
-        for (int i = 0; i < wordLength; i++) {
-            if (guessed[i] == 0) {
-                allGuessed = 0;
-                break;
-            }
-        }
-
-        if (allGuessed) {
-            printf("Congratulations! You guessed the word: %s\n", word);
+        // Check if the player has guessed the whole word
+        if (strcmp(selectedWord.word, guessedWord) == 0) {
+            printf("Congratulations! You guessed the word: %s\n", selectedWord.word);
+            gameWon = 1;
             break;
         }
     }
 
-    if (wrongGuesses == MAX_TRIES) {
-        printf("You lost! The word was: %s\n", word);
+    if (!gameWon) {
+        printf("\nGame Over! The word was: %s\n", selectedWord.word);
     }
 
     return 0;
